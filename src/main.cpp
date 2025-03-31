6effa7a0f8c298d38616e75699cb28444488b12a
m-lm <31556425+m-lm@users.noreply.github.com>
2025-03-31 02:35:17 -0500

    Update tests

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,18 @@ void test() {
     db.put("Sally", "Red");
     db.put("James", "Purple");
     db.display();
+
+    auto value = db.get("Sally");
+    if (value) {
+        std::cout << *value << std::endl;
+    }
+
+    db.del("Sally");
+    value = db.get("Sally");
+    if (value) {
+        std::cout << *value << std::endl;
+    }
+    db.display();
 }
 
 int main() {
